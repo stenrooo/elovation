1179b676392f180922e26f284e04a27f61a671c3
Olli Stenroos <olli.stenroos@entteri.fi>
2022-02-01 15:48:03 +0200

    Try fix build

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+config.assets.precompile += %w( *.css.sass )
+config.assets.precompile += %w( *.css.scss )
+config.assets.precompile += %w( *.js )
+config.assets.precompile += %w( *.js.coffee )
+config.assets.precompile += %w( *.js.coffee.erb )
